5845286b12ae5e58c1340d4ed05f7a071b27146c
Mayank Badola <badola21295@gmail.com>
2016-10-01 22:53:15 +0530

    updated script

--- service.rb
+++ service.rb
@@ -1,22 +1,32 @@
 require 'sinatra'
-<<<<<<< HEAD
 require "http"
 require 'json'
 require "base64"
 
+client_id = 'staging-grofers'
+client_secret = '51e6d096-56f6-40b4-a2b9-9e0f8fa704b8'
+
+registered_users = {
+	"9810181713" => '77cb89f5-35da-4011-92f1-deffd0972200',
+	"7838121295" => 'e7fdbe44-2c8d-43ca-8c17-269b2d78cdfa'
+}
+
 get '/' do
 	# content_type :json
-	mobile_number = params['mobile']
+	mobile_number = params['from']
 	message = params['message']
 	tokens = message.split()
-	
 	if tokens[0].downcase == "paytm"
+		puts "Message from "<<mobile_number<<"\nQuery : "<<message
+		"""
 		case tokens[1]
-		when "send", "pay"
-			puts "call API 1"
-		when "balance", "bal"
-			puts "call API 2" 
+		when 'send', 'pay'
+			puts 'call API 1'
+		when 'balance', 'bal'
+			puts 'call API 2'
 		end
+		"""
+		"all ok"
 	end
 
 	# tokens.length.times do | token |
@@ -24,4 +34,82 @@ get '/' do
 	# end
 	# tokens
 	# { :message => "#{params['message']}"}.to_json
+end
+
+get '/getState' do
+	email = params['email']
+	phone = params['mobile_number']
+	get_state_hash = {
+		:email => email,
+		:phone => phone,
+		:clientId => client_id,
+		:scope => 'wallet',
+		:responseType => 'token'
+	}
+	response = HTTP.post("https://accounts-uat.paytm.com/signin/otp", :body => get_state_hash.to_json)
+	if response.code != 200
+		"Failed response"
+	else
+		response.body
+	end
+end
+
+get '/getToken' do
+	basic_auth = "Basic "<<Base64.strict_encode64("#{client_id}:#{client_secret}")
+	print basic_auth
+	otp = params['otp']
+	state = params['state']
+	get_token_hash = {
+		:otp => otp,
+		:state => state
+	}
+	print get_token_hash.to_json
+	response = HTTP.headers("Authorization" => basic_auth, "Content-Type" => 'application/json').post('https://accounts-uat.paytm.com/signin/validate/otp', :body => get_token_hash.to_json)
+	if response.code != 200
+		"Failed token request"
+	else
+		response.body
+	end
+end
+
+get '/transfer' do
+	from = params['from']
+	to = params['to']
+	amt = params['amt'].to_i
+
+	query = {
+		:request => {
+			:isToVerify => 0,
+			:isLimitApplicable => 0,
+			:payeeEmail => "",
+			:payeeMobile => to,
+			:payeeCustId => "",
+			:amount => amt,
+			:currencyCode => "INR",
+			:comment => "Loan"
+		},
+		:ipAddress => "127.0.0.1",
+		:platformName => "PayTM",
+		:operationType => "P2P_TRANSFER"
+	}
+
+	response = HTTP.headers(:ssotoken => registered_users[from]).post('https://trust-uat.paytm.in/wallet-web/wrapper/p2pTransfer', :body => query.to_json)
+	if response.code != 200
+		"Failed Transfer request"
+	else
+		response.body
+	end
+end
+
+get '/checkBal' do
+	#of = params['of']
+	# trust-uat
+	#puts registered_users[of]<<"\n"
+	check_balance_api_url = 'https://trust-uat.paytm.in/wallet-web/checkBalance'
+	response = HTTP.headers(:ssotoken => "e7fdbe44-2c8d-43ca-8c17-269b2d78cdfa").post(check_balance_api_url)
+	if response.code != 200
+		"Failed bal req #{response.code}"
+	else
+		response.body
+	end
 end
