2604a6f95c240460c7240cbf6668191838ea5af8
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-10-02 04:33:33 +0530

    validation flow complete + redis integration

--- service.rb
+++ service.rb
@@ -3,12 +3,16 @@ require "http"
 require 'json'
 require "base64"
 require 'twilio-ruby'
+require "redis"
 
 client_id = 'staging-hackathalon'
 client_secret = 'ac2c2f5b-cea8-4c62-9b28-a6de77e221df'
+service_name = "Dove"
+
+redis = Redis.new
 
 registered_users = {
-	"9810181713" => '77cb89f5-35da-4011-92f1-deffd0972200',
+	# "9810181713" => '77cb89f5-35da-4011-92f1-deffd0972200',
 	"7838121295" => 'e7fdbe44-2c8d-43ca-8c17-269b2d78cdfa'
 }
 
@@ -25,15 +29,15 @@ def send_twilio_message(to_number, message)
 	})
 end
 
-def checkBal(me, registered_users)
+def check_bal(redis, me, registered_users)
 	sms_message = nil
 
 	check_balance_api_url = 'https://trust-uat.paytm.in/wallet-web/checkBalance'
 	me.slice! "+91"
 	
-	if registered_users.key?(me)
-		user_oauth = registered_users[me]
-		response = HTTP.headers(:ssotoken => user_oauth).post(check_balance_api_url)
+	user_token = redis.get("user_tokens:#{me}")
+	if user_token
+		response = HTTP.headers(:ssotoken => user_token).post(check_balance_api_url)
 
 		if response.code != 200
 			puts "Failed Request | #{response.code}"
@@ -43,13 +47,68 @@ def checkBal(me, registered_users)
 			sms_message = "\nHi!\nYour wallet balance is : " + amount.to_s
 		end
 	else
-		sms_message = "\nSorry! We don't have any account associated with +91-" + me
+		sms_message = "\nSorry! We don't have any account associated with +91-" + me + "\nSend 'paytm reg <email>' to register your number"
 	end
 	
 	if sms_message
 		send_twilio_message("+91" + me, sms_message)
 	end
 end
+
+def reg_user(redis, mobile_number, email, client_id)
+	sms_message = nil
+	
+	register_user_api_url = "https://accounts-uat.paytm.com/signin/otp"
+	mobile_number.slice! "+91"
+
+	get_state_hash = {
+		:email => email,
+		:phone => mobile_number,
+		:clientId => client_id,
+		:scope => 'wallet',
+		:responseType => 'token'
+	}
+
+	response = HTTP.post(register_user_api_url, :body => get_state_hash.to_json)
+	if response.code != 200
+		puts "Failed Request | #{response.code}"
+	else
+		sms_message = "\nFinal Step! Send the OTP received as 'paytm validate <OTP>' from your registered mobile number"
+		redis_key = "validate_state:" + mobile_number
+		json_response = JSON.parse(response.body)
+		redis.set(redis_key, json_response['state'])
+	end
+
+	if sms_message
+		send_twilio_message("+91" + mobile_number, sms_message)
+	end
+end
+
+def validate_user(redis, client_id, client_secret, mobile_number, user_otp)
+	sms_message = nil
+
+	validate_user_api_url = "https://accounts-uat.paytm.com/signin/validate/otp"
+	mobile_number.slice! "+91"
+
+	basic_auth = "Basic "<<Base64.strict_encode64("#{client_id}:#{client_secret}")
+	get_token_hash = {
+		:otp => user_otp,
+		:state => redis.get("validate_state:#{mobile_number}")
+	}
+	puts get_token_hash[:state]
+	puts get_token_hash[:state].class
+	response = HTTP.headers("Authorization" => basic_auth, "Content-Type" => 'application/json').post(validate_user_api_url, :body => get_token_hash.to_json)
+	if response.code != 200
+		puts "Failed Request | #{response.code}"
+	else
+		sms_message = "\nCongrats\n You can now use Dove!"
+		json_response = JSON.parse(response.body)
+		puts json_response["access_token"]
+		puts response.body
+		redis.set("user_tokens:#{mobile_number}", json_response["access_token"])
+	end
+end
+
 
 get '/' do
 	mobile_number = params['from']
@@ -62,46 +121,22 @@ get '/' do
 			puts 'call API 1'
 		when 'balance', 'bal'
 			puts "entering balance"
-			checkBal(mobile_number, registered_users)
+			check_bal(redis, mobile_number, registered_users)
+		when 'register', 'reg'
+			puts "entering registration"
+			email = tokens[2].downcase
+			reg_user(redis, mobile_number, email, client_id)
+		when 'validate'
+			puts "entering registration step 2"
+			user_otp = tokens[2].downcase
+			validate_user(redis, client_id, client_secret, mobile_number, user_otp)
 		end
 	end
-	"Flow Succesful"
-end
-
-get '/getState' do
-	email = params['email']
-	phone = params['mobile_number']
-	get_state_hash = {
-		:email => email,
-		:phone => phone,
-		:clientId => client_id,
-		:scope => 'wallet',
-		:responseType => 'token'
-	}
-	response = HTTP.post("https://accounts-uat.paytm.com/signin/otp", :body => get_state_hash.to_json)
-	if response.code != 200
-		"Failed response"
-	else
-		response.body
-	end
+	"YOLO | Flow Succesful"
 end
 
 get '/getToken' do
-	basic_auth = "Basic "<<Base64.strict_encode64("#{client_id}:#{client_secret}")
-	print basic_auth
-	otp = params['otp']
-	state = params['state']
-	get_token_hash = {
-		:otp => otp,
-		:state => state
-	}
-	print get_token_hash.to_json
-	response = HTTP.headers("Authorization" => basic_auth, "Content-Type" => 'application/json').post('https://accounts-uat.paytm.com/signin/validate/otp', :body => get_token_hash.to_json)
-	if response.code != 200
-		"Failed token request"
-	else
-		response.body
-	end
+	
 end
 
 get '/transfer' do
